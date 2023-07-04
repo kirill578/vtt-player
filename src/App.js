2f2c2aee530315f2432f5c89631f40900035c5c3
Kirill Kulakov <Kirill578@gmail.com>
2023-07-04 15:43:10 -0500

    feat

--- src/App.js
+++ src/App.js
@@ -5,9 +5,12 @@ import React, { useState } from 'react';
 
 
 function App() {
+  const player = React.useRef(null);
   const [mp3File, setMp3File] = useState('');
+  const [mp3Filename, setMp3Filename] = useState('');
   const [vttFile, setVttFile] = useState('');
   const vttInputRef = React.useRef(null);
+  const [bookmarks, setBookmarks] = React.useState([]);
 
   const handleMp3Change = (event) => {
     const reader = new FileReader();
@@ -17,6 +20,15 @@ function App() {
       setVttFile('');
       vttInputRef.current.value = null;
       document.title = event.target.files[0].name;
+      setMp3Filename(event.target.files[0].name);
+      debugger;
+      try {
+        const rawBookmarks = localStorage[`bookmarks-${event.target.files[0].name}`];
+        const newBookmarks = JSON.parse(rawBookmarks);
+        setBookmarks(newBookmarks ?? []);
+      } catch (e) {
+        console.error(e);
+      }
     };
     reader.readAsDataURL(event.target.files[0]);
   };
@@ -30,6 +42,55 @@ function App() {
     reader.readAsDataURL(event.target.files[0]);
   };
 
+  React.useEffect(() => {
+    const handleKeyPress = (event) => {
+      if (event.code === 'Space') {
+        event.preventDefault();
+        if (player.current.audio.current.paused) {
+          player.current.audio.current.play();
+        } else {
+          player.current.audio.current.pause();
+        }
+      } else if (event.code === 'ArrowRight') {
+        event.preventDefault();
+        player.current.audio.current.currentTime += 2;
+      } else if (event.code === 'ArrowLeft') {
+        event.preventDefault();
+        player.current.audio.current.currentTime -= 2;
+      } else if (event.code === 'ArrowUp') {
+        event.preventDefault();
+        player.current.audio.current.currentTime -= 15;
+      } else if (event.code === 'ArrowDown') {
+        event.preventDefault();
+        player.current.audio.current.currentTime += 15;
+      } else if (event.ctrlKey && event.code >= 'Digit1' && event.code <= 'Digit9') {
+        event.preventDefault();
+        const bookmarkIndex = Number(event.code.slice(-1)) - 1;
+        if (bookmarkIndex >= 0 && bookmarkIndex < 9) {
+          const currentTime = player.current.audio.current.currentTime;
+          const newBookmarks = [...bookmarks];
+          newBookmarks[bookmarkIndex] = currentTime;
+          setBookmarks(newBookmarks);
+          debugger;
+          localStorage.setItem(`bookmarks-${mp3Filename}`, JSON.stringify(newBookmarks))
+        }
+      } else if (event.code >= 'Digit1' && event.code <= 'Digit9') {
+        event.preventDefault();
+        const bookmarkIndex = Number(event.code.slice(-1)) - 1;
+        if (bookmarkIndex >= 0 && bookmarkIndex < 9 && bookmarks[bookmarkIndex]) {
+          const bookmarkTime = bookmarks[bookmarkIndex];
+          player.current.audio.current.currentTime = bookmarkTime;
+        }
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyPress);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyPress);
+    };
+  }, [bookmarks, mp3Filename]);
+
   return (
     <div className="App">
       <label htmlFor="mp3Input">MP3 File:</label>
@@ -38,7 +99,22 @@ function App() {
       <label htmlFor="vttInput">VTT File:</label>
       <input type="file" id="vttInput" accept=".vtt"  onChange={handleVttChange} ref={vttInputRef} />
 
+      <div className="timestamps">
+        <div className="timestamp">CTRL+1..9</div>
+        {bookmarks.map((timestamp, index) => {
+          const minutes = Math.floor(timestamp / 60);
+          const seconds = timestamp % 60;
+          const formattedTimestamp = `${minutes}:${Math.floor(seconds.toString().padStart(2, '0'))}`;
+
+          return (
+            <div key={index} className="timestamp" onClick={() => player.current.audio.current.currentTime = timestamp}>
+              <b>{index + 1}</b>   {formattedTimestamp}
+            </div>
+          );
+        })}
+      </div>
       {mp3File && vttFile && <Player
+        ref={player}
         audio={mp3File}
         transcript={vttFile}
       />}
