4c1cff70371058c322f6f6f5d51400bfa97e10e0
Kirill Kulakov <Kirill578@gmail.com>
2023-07-04 14:45:27 -0500

    wip

--- src/App.js
+++ src/App.js
@@ -7,12 +7,16 @@ import React, { useState } from 'react';
 function App() {
   const [mp3File, setMp3File] = useState('');
   const [vttFile, setVttFile] = useState('');
+  const vttInputRef = React.useRef(null);
 
   const handleMp3Change = (event) => {
     const reader = new FileReader();
     reader.onload = () => {
       const mp3Data = reader.result.split(',')[1];
       setMp3File('data:audio/mp3;base64,' + mp3Data);
+      setVttFile('');
+      vttInputRef.current.value = null;
+      document.title = event.target.files[0].name;
     };
     reader.readAsDataURL(event.target.files[0]);
   };
@@ -29,10 +33,10 @@ function App() {
   return (
     <div className="App">
       <label htmlFor="mp3Input">MP3 File:</label>
-      <input type="file" id="mp3Input" accept=".mp3" onChange={handleMp3Change} />
+      <input type="file" id="mp3Input" accept=".mp3,.m4a,.mp4" onChange={handleMp3Change} />
 
       <label htmlFor="vttInput">VTT File:</label>
-      <input type="file" id="vttInput" accept=".vtt" onChange={handleVttChange} />
+      <input type="file" id="vttInput" accept=".vtt"  onChange={handleVttChange} ref={vttInputRef} />
 
       {mp3File && vttFile && <Player
         audio={mp3File}
